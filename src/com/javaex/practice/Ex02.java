855cdfec208e207abceacd136b333e4baad2d66e
jnnd24 <jnnd24@naver.com>
2022-04-20 18:15:17 +0900

    완료

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -1,13 +1,13 @@
 package com.javaex.practice;
 
-public class Ex02미완 {
+public class Ex02 {
 
 	public static void main(String[] args) {
 
 		double[] arr = new double[] {1.2, 3.3, 6.7};
 		
 		for(int i = 0; i<arr.length; i++) {
-			System.out.println(arr[i]);
+			System.out.println(arr[2-i]);
 		}
 		
 		
